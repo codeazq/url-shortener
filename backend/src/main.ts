8e36d05ee47bc8842de505d80b0d09b5e22651eb
codeazq <ebiyomarejonathan@gmail.com>
2023-05-22 16:15:34 +0100

    fix request validation error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Url Shortner API')
