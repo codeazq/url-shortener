920d2a900446bd9700c716dec9cd1a1768c12361
codeazq <ebiyomarejonathan@gmail.com>
2023-05-29 06:14:11 +0100

    crate short-link using axios

--- frontend/app/create-short-link/page.jsx
+++ frontend/app/create-short-link/page.jsx
@@ -3,28 +3,35 @@
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 import Form from "@components/Form";
 
 const CreateShortLink = () => {
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({ url: "", alias: "" });
+  const { data: session } = useSession();
 
   const createShortLink = async (e) => {
     e.preventDefault();
     setSubmitting(true);
 
     try {
-      const response = await fetch("http://localhost:4000/short-links", {
-        method: "POST",
-        body: JSON.stringify({
+      const response = axios.post(
+        `${process.env.BACKEND_URL}/short-links`,
+        {
           url: post.url,
           alias: post.alias,
-        }),
-        headers: { Authentication: "Bearer {token}" },
-      });
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+        }
+      );
 
-      if (response.ok) Router.push("/");
+      data = response.data;
     } catch (error) {
       console.log(error);
     } finally {
